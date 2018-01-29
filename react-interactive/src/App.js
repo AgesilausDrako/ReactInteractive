84bd613204687ba817e6c24a920932a97894f7e0
AgesilausDrako <agesilaus@yahoo.com>
2018-01-29 16:46:51 -0500

    added new rule functionality

--- react-interactive/src/App.js
+++ react-interactive/src/App.js
@@ -20,6 +20,42 @@ class App extends Component {
     })
   }
 
+  rules = () => {
+    let {rules} = this.state
+    let array = []
+    let fields = ['name', 'begin', 'end']
+    for (let i=0; i<rules; i++) {
+      array.push(
+        <Row
+          key={i}>
+          <Column>
+            {fields.map( (field, index) => {
+              return (
+                <Column
+                key={index}>
+                  <RuleLabel>
+                    {field}
+                  </RuleLabel>
+                  <RuleInput
+                    value={this.state[`${field}${i}`]}
+                    onChange={this.handleChange}
+                    name={`${field}${i}`}
+                  />
+                </Column>
+              )
+            })}
+          </Column>
+          <StyleInput
+            value={this.state[`style${i}`]}
+            onChange={this.handleChange}
+            name={`style${i}`}
+          />
+        </Row>
+      )
+    }
+    return array
+  }
+
   newFields = () => {
     this.setState( (prevState) => {
       let {rules} = prevState
@@ -45,10 +81,11 @@ class App extends Component {
 
   render() {
     let {value} = this.state
-    let {handleChange, newFields} = this
+    let {handleChange, newFields, rules} = this
     return (
       <Container>
         <Column>
+          {rules()}
           <Button
             onClick={newFields}
           >
